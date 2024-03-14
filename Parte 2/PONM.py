035822d5af27c14a7033ea4ce3b7aba168a988b1
GabrielVL <gavalo.99@gmail.com>
2024-03-13 20:27:22 -0600

    Creación de PONM funcional

--- Parte 2/PONM.py
+++ Parte 2/PONM.py
@@ -1,76 +1,80 @@
-import math
+import sympy as sp
+import numpy as np
+import time
 
-from sympy import *
-from time import time
+
+# Función para derivadas numéricas
+def derivative_numeric(f, xo, h=1e-6):
+    # Incrementar el valor de h si es demasiado pequeño
+    h = max(h, np.finfo(float).eps ** (1/3))
+    return (f(xo + h) - f(xo)) / h
 
 
+# Función principal del método PONM
 def PONM(xo, func, tol, iterMax):
-    # Aproximación de cero de la función func utilizando el método de llamado Optimal iterative algorithm with
-    # four-order accuracy (PONM)
-    # Estructura: [vi, xk, error, k, time] = PONM(xo, func, tol, iterMax)
-
-    # Parámetros: xo = indica el valor donde se comienza (también representado como vi)
-    # func = función a la que se le aproxima
-    # tol = tolerancia de aproximación
-    # iterMax = iteraciones máximas a realizar
-    # xk = aproximacion del cero
-    # error = error del metodo dado por | func(xk) |
-    # k = iteraciones realizada
-
-    # Guardamos el valor inicial
+    # Inicialización de variables y tiempo
     vi = xo
+    tiempo_inicial = time.time()
+    x = sp.symbols('x')
+    f = sp.sympify(func)
 
-    # Comenzamos a tomar el tiempo
-    tiempo_inicial = time()
+    # Convertir la función a una función lambda
+    f_func = sp.lambdify(x, f)
 
-    # Tomar el string y convertirlo a una expresion que se pueda usar como funcion
-    x = symbols('x')
-    f = Function(func)(x)
+    # Iteraciones del método
+    for k in range(1, iterMax + 1):
+        # Evaluación de la función en xo
+        funcEvaluatedXn = f_func(xo)
+        # Evaluación de la derivada numérica en xo
+        derivedfEvaluatedXn = derivative_numeric(f_func, xo)
+        # Cálculo de yn
+        yn = xo - funcEvaluatedXn / derivedfEvaluatedXn
+        # Evaluación de la función en yn
+        funcEvaluatedYn = f_func(yn)
 
-    # Se deriva la función
-    derivedSym = f.diff(x)
-    # Pasamos de una expresión symbolic a una función tanto la función derivada como la función a utilizar
-    # derivedf = function _handle(derivedSym)
-    # func = function_handle(f)
-
-    for k in range(iterMax):
-        # Sustituir el calor de x con el de x0 en la función dericada
-        derivedEvaluatedXn = derivedSym.subs(x, xo)
-        # Sustituir el valor de x con el de x0 en la función original
-        funcEvaluatedXn = f.subs(x, xo)
-        # Obtenemos el valor de yn
-        yn = xo - (funcEvaluatedXn / derivedEvaluatedXn)
-        # Calculamos el valor de la función func con el valor de yn
-        funcEvaluatedYn = f.subs(x, yn)
-        if k == 0:
-            xk = xo - ((funcEvaluatedXn * (funcEvaluatedXn - (2 * funcEvaluatedYn)) - (funcEvaluatedYn ** 2)) /
-                       (derivedEvaluatedXn * (funcEvaluatedXn - 3 * funcEvaluatedYn)))
+        # Primer paso: calcular xk y xk-1
+        if k == 1:
+            nominator = funcEvaluatedXn * (funcEvaluatedXn - 2 * funcEvaluatedYn) - funcEvaluatedYn ** 2
+            denominator = derivedfEvaluatedXn * (funcEvaluatedXn - 3 * funcEvaluatedYn)
+            xk = xo - (nominator / denominator)
             xkMinusOne = xk
         else:
+            # Cálculo de xk y xk-1
             xkMinusOne = xk
-            xk = xo - ((funcEvaluatedXn * (funcEvaluatedXn - (2 * funcEvaluatedYn)) - (funcEvaluatedYn ** 2)) /
-                       (derivedEvaluatedXn * (funcEvaluatedXn - 3 * funcEvaluatedYn)))
-            # Después de varias pruebas se notó que xk después de ciertas iteraciones para varias funciones genera NaN,
-            # entonces se decidió agregar que el momento que esto ocurra se detenga la ejecución. Aunque no se
-            # determinó la causa exacta se cree que es porque los valores por ser tan similares y pequeños generan un
-            # cero en el denominador causando la indefinición.
-            # if math.isnan(xk):
-            #     # Terminamos de contar el tiempo si se detecta una indefinición en el valor de xk
-            #      tiempo_final = time() - tiempo_inicial
-            #      xk = xkMinusOne
-            #      break
-        error = max(abs(f.subs(x, xk)), abs(xk - xkMinusOne))
-        # print(lambdify(x, error, "math"))
+            nominator = funcEvaluatedXn * (funcEvaluatedXn - 2 * funcEvaluatedYn) - funcEvaluatedYn ** 2
+            denominator = derivedfEvaluatedXn * (funcEvaluatedXn - 3 * funcEvaluatedYn)
+            xk = xo - (nominator / denominator)
+
+            # Manejo de valores complejos
+            if isinstance(xk, complex) or abs(xk.imag) > 0:
+                tiempo = time.time() - tiempo_inicial
+                xk = xkMinusOne
+                break
+
+        # Cálculo del error
+        error = max(abs(f_func(xk)), abs(xk - xkMinusOne))
+
+        # Comprobación de la tolerancia
         if error < tol:
-            # Terminamos de contar el tiempo si llega al error aceptable
-            tiempo_final = time() - tiempo_inicial
+            tiempo = time.time() - tiempo_inicial
             break
         else:
             xo = xk
-    tiempo_final = time() - tiempo_inicial
-    return vi, xk, error, k, tiempo_final
 
+    # Tiempo total de ejecución
+    tiempo = time.time() - tiempo_inicial
 
-x = symbols('x')
-print(PONM(3, 'x**2', 1000, 9000))
+    return vi, xk, error, k, tiempo
 
+# Ejemplo de uso
+xo = 2
+func = '1-(16*sqrt(x)/7)+(4*x/3)-(1*(x**4)/21)'
+tol = 1e-10
+iterMax = 10
+
+vi, xk, error, k, tiempo_final = PONM(xo, func, tol, iterMax)
+print("Aproximación inicial:", vi)
+print("Aproximación del cero:", xk)
+print("Error:", error)
+print("Iteraciones realizadas:", k)
+print("Tiempo total:", tiempo_final)
